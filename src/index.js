8e05617b49c64be3d153e9df6b68f8a33f7d643e
Jueun <jueunsuh@gmail.com>
2022-10-06 16:50:32 +0900

    removed >>>Head

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { createStore } from 'redux';
 const form = document.querySelector("form");
 const input = document.querySelector("input");
@@ -28,48 +27,3 @@ const onSubmit = e => {
 };
 
 form.addEventListener("submit", onSubmit);
-=======
-import { createStore } from "redux";
-
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
-
-number.innerText = 0;
-
-const ADD = "ADD";
-const MINUS = "MINUS";
-
-
-const countModifier = (count = 0, action) => {
-  switch (action.type) {
-    case ADD:
-      return count + 1;
-    case MINUS:
-      return count - 1;
-    default:
-      return count;
-  }
-};
-
-const countStore = createStore(countModifier);
-
-const onChange = () => {
-  number.innerText = countStore.getState();
-};
-
-countStore.subscribe(onChange);
-
-const handleAdd = () => {
-  countStore.dispatch({ type: ADD })
-}
-
-const handleMinus = () => {
-  countStore.dispatch({ type: MINUS })
-}
-
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
-
->>>>>>> 2dd3744ce41e948391eaf4687a1c8c3e952a1313
-
